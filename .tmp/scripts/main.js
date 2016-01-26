fd1f1dac04ecfa1193263e404b96b5a1fd5a5b9c
Pedro Poveda <pedropovedaq@gmail.com>
2016-01-26 04:17:09 -0500

    hello horizontal scroll

--- .tmp/scripts/main.js
+++ .tmp/scripts/main.js
@@ -1,20 +1,14 @@
 'use strict';
 
 $(document).ready(function () {
-    $(function () {
-        $('ul.nav a').bind('click', function (event) {
-            var $anchor = $(this);
-            /*
-            if you want to use one of the easing effects:
-            $('html, body').stop().animate({
-                scrollLeft: $($anchor.attr('href')).offset().left
-            }, 1500,'easeInOutExpo');
-             */
-            $('html, body').stop().animate({
-                scrollLeft: $($anchor.attr('href')).offset().left
-            }, 1000);
-            event.preventDefault();
-        });
-    });
+    (function ($) {
+        $.jInvertScroll(['.fullpage'], {
+            width: 'auto', // Page width (auto or int value)
+            height: 'auto' });
+    })( // Page height (the shorter, the faster the scroll)
+
+    jQuery);
+
+    console.log('HI!');
 });
 //# sourceMappingURL=main.js.map
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,19 +1,14 @@
- $(document).ready(function() {
-     $(function() {
-         $('ul.nav a').bind('click', function(event) {
-             var $anchor = $(this);
-             /*
-             if you want to use one of the easing effects:
-             $('html, body').stop().animate({
-                 scrollLeft: $($anchor.attr('href')).offset().left
-             }, 1500,'easeInOutExpo');
-              */
-             $('html, body').stop().animate({
-                 scrollLeft: $($anchor.attr('href')).offset().left
-             }, 1000);
-             event.preventDefault();
-         });
-     });
- });
+$(document).ready(function () {
+    (function($) {
+        $.jInvertScroll(['.section1', '.section2' '.section3'], {
+    
+    
+});
 
- 
+
+
+    }(jQuery));
+
+    console.log('HI!')
+});
+
