7d0571271285200c970dd6bd22b896089eaa6d55
Pedro Poveda <pedropovedaq@gmail.com>
2017-08-01 23:06:35 -0400

    Initial dist subtree commit

---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+'use strict';
+
+$(document).ready(function() {
+
+    // var width= window.innerHeight*16.35;
+
+    // $(".fullpage").width(width);
+
+    (function($) {
+        $.jInvertScroll(['.fullpage'], {});
+    }(jQuery));
+
+
+    $(window).bind('resize', function() {
+
+
+
+        (function($) {
+            $.jInvertScroll(['.fullpage'], {});
+        }(jQuery));
+
+    });
+});
